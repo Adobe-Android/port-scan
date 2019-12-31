96d4252ac4f8fcece62be929f59968230d8a845f
Adobe-Android <tngamerboy@gmail.com>
2019-12-30 20:40:23 -0600

    Small formatting changes

--- src/port_scan.py
+++ src/port_scan.py
@@ -7,13 +7,16 @@ def main():
     print("##############################################")
     print("| Welcome to the Simple Python Port Scanner. |")
     print("##############################################")
-    target = input("[+] Enter Target IP: ")
-    print("Choose one of the following options below.")
-    print("(1) Specify a single port")
-    print("(2) Specify a port range")
-    print("(3) Specify a list of ports")
-    menu_opt1 = input("Your choice: ")
-    menu_opt2 = input("What port(s) would you like to scan? ")
+    target = input(" [+] Enter Target IP: ")
+    print("##############################################")
+    print("| Choose one of the following options below. |")
+    print("| (1.) Specify a single port                 |")
+    print("| (2.) Specify a port range                  |")
+    print("| (3.) Specify a list of ports               |")
+    print("##############################################")
+    menu_opt1 = input("  Your choice: ")
+    menu_opt2 = input("  What port(s) would you like to scan? ")
+    print("##############################################")
 
     if menu_opt1 == "1":
         single_port(target, menu_opt2)
@@ -26,11 +29,11 @@ def main():
 
 def single_port(target, port):
     for portNumber in range(int(port), int(port) + 1):
-        print("Scanning port", portNumber)
+        print("Scanning port", portNumber, 'on IP address', target)
         if scanner(target, portNumber):
-            print('[*] Port', portNumber, '/tcp','is open')
+            print('  [*] Port', portNumber, '/tcp','is open')
         else:
-            print('[*] Port', portNumber, '/tcp','is closed')
+            print('  [*] Port', portNumber, '/tcp','is closed')
 
 def scanner(target, port):
     try:
