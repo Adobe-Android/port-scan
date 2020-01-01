25fc96ed2cd564583787eae9f00fd0525d94a65e
Adobe-Android <tngamerboy@gmail.com>
2020-01-01 12:59:28 -0600

    UX improvements, added option 3 functionality

--- src/port_scan.py
+++ src/port_scan.py
@@ -1,15 +1,36 @@
 #!/usr/bin/env python
 
 import socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+import os
+import time
+import sys
+
+# Useful option for showing connection exceptions
+debug_mode = False
 
 def main():
     # Allows for graceful program exit when using Ctrl-C
     try:
+        menu()
         while True:
-            menu()
+            continue_program = input("  [+] Would you like to continue? (Y/N): ").lower()
+            if continue_program == 'y' or continue_program == 'yes':
+                clear()
+                menu()                
+            else:
+                sys.exit()
     except KeyboardInterrupt:
         pass
+
+def clear(): 
+  
+    # For Windows NT based systems
+    if os.name == 'nt': 
+        _ = os.system('cls') 
+  
+    # For POSIX systems (Linux, macOS, FreeBSD, OpenBSD, NetBSD, and more) 
+    else: 
+        _ = os.system('clear') 
 
 def menu():
     print("##############################################")
@@ -26,23 +47,36 @@ def menu():
     menu_opt1 = input("  Your choice: ")
     menu_opt2 = input("  What port(s) would you like to scan? ")
     print("##############################################")
+    clear()
 
     if menu_opt1 == "1":
         end_position = int(menu_opt2) + 1
-        test_ports(target, menu_opt2, end_position)
+        test_ports_by_range(target, menu_opt2, end_position)
     elif menu_opt1 == "2":
-        text = menu_opt2.split(',')
-        start_position = int(text[0].strip())
-        end_position = int(text[-1].strip()) + 1
-        test_ports(target, start_position, end_position)
+        tmp_str = menu_opt2.split(',')
+        start_position = int(tmp_str[0].strip())
+        end_position = int(tmp_str[-1].strip()) + 1
+        test_ports_by_range(target, start_position, end_position)
     elif menu_opt1 == "3":
-        pass
+        port_list = menu_opt2.split(',')
+        trimmed_port_list = list(map(str.strip, port_list))
+        test_specific_ports(target, trimmed_port_list)
     else:
         print("Well...that was unexpected.")
+        print("Please choose from one of the options above.")
 
-def test_ports(target, port, end_position):
+def test_specific_ports(target, port_list):
     
-    for portNumber in range(int(port), end_position):
+    for portNumber in port_list:
+        print("Scanning port", portNumber, 'on IP address', target)
+        if scanner(target, int(portNumber)):
+            print('  [*] Port', portNumber, '/tcp','is open')
+        else:
+            print('  [*] Port', portNumber, '/tcp','is closed')
+
+def test_ports_by_range(target, start_position, end_position):
+    
+    for portNumber in range(int(start_position), end_position):
         print("Scanning port", portNumber, 'on IP address', target)
         if scanner(target, portNumber):
             print('  [*] Port', portNumber, '/tcp','is open')
@@ -51,9 +85,15 @@ def test_ports(target, port, end_position):
 
 def scanner(target, port):
     try:
+        # Properly creates a new socket for each test connection
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((target, port))
+        sock.shutdown(socket.SHUT_RDWR)
+        sock.close()
         return True
-    except:
+    except Exception as e:
+        if debug_mode == True:
+            print(e)
         return False
 
 if __name__ == "__main__":
